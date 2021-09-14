07f0b425164c54de78b5130ebaf4bd0fc7cc6fd4
Tony Dawson <tony.dawson@microlise.com>
2021-09-14 15:55:04 +0100

    Added TonyController

---
+++ OsDemoApi/Controllers/TonyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OsDemoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TonyController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            // Get the local computer host name.
+            var hostName = Dns.GetHostName();
+
+            return "Computer name :" + hostName;
+        }
+    }
+
+}
