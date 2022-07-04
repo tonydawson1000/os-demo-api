65fc48a0905848500ac41cc477bd8571a1e4e397
Tony Dawson <tony.dawson@microlise.com>
2022-07-04 16:06:35 +0100

    Added some Log Messages to TonyController

--- OsDemoApi/Controllers/TonyController.cs
+++ OsDemoApi/Controllers/TonyController.cs
@@ -6,9 +6,23 @@ namespace OsDemoApi.Controllers
     [Route("[controller]")]
     public class TonyController : ControllerBase
     {
+        private readonly ILogger<TonyController> _logger;
+
+        public TonyController(ILogger<TonyController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public string Get()
         {
+            _logger.LogDebug("Hello a DEBUG Messsage");
+
+            _logger.LogInformation("Hello an INFO Message");
+
+
+            _logger.LogError("Hello an ERROR Message");
+
             // Get the 'pod' host name.
             return $"Hello World - ITS FRIDAY - Host: '{Environment.MachineName}' - {DateTime.UtcNow}";
         }
